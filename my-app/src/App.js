830f6985dba092098c2a371d757230f1fa9841a1
RomanGustaytis <roman.gustaytis@gmail.com>
2021-11-13 18:40:13 +0200

    add src files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import Header from "./partials/Header/Header";
+import Content from "./partials/Content/Content";
+import Asside from "./partials/Asside/Asside";
+
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
+      <Asside />
+      <Content isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
     </div>
   );
 }
